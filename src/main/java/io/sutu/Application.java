2fe0d1ed2cb19022a6b35cf04139df6b0ef20c17
Dragos Sutu <dragos.sutu@gmail.com>
2017-08-31 21:55:08 +0300

    Initial commit. Many more will follow.

---
+++ src/main/java/io/sutu/Application.java
@@ -0,0 +1,8 @@
+package io.sutu;
+
+class Application {
+
+    void run() {
+        System.out.println("EUREKA !");
+    }
+}
---
+++ src/main/java/io/sutu/Warren.java
@@ -0,0 +1,8 @@
+package io.sutu;
+
+class Warren {
+    public static void main(String[] args) {
+        Application app = new Application();
+        app.run();
+    }
+}
