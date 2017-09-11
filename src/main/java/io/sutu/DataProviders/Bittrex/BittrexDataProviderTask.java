bf58ecdf17e16cb9e68bf5637b34fb079f5567a4
Dragos Sutu <dragos.sutu@gmail.com>
2017-09-11 08:16:32 +0300

    Set storage through setter method, parse json response.

--- src/main/java/io/sutu/DataProviders/Bittrex/BittrexDataProviderTask.java
+++ src/main/java/io/sutu/DataProviders/Bittrex/BittrexDataProviderTask.java
@@ -1,10 +1,13 @@
 package io.sutu.DataProviders.Bittrex;
 
+import com.google.gson.Gson;
 import io.sutu.DataProviders.Bittrex.Communications.BittrexClient;
 import io.sutu.DataProviders.Bittrex.Communications.BittrexClientException;
-import io.sutu.Storage;
+import io.sutu.Storage.Storage;
+import io.sutu.Storage.Ticker;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
 import java.util.Date;
 
 @Component
@@ -12,13 +15,20 @@ public class BittrexDataProviderTask implements Runnable {
 
     private final BittrexClient bittrexClient;
 
-    private final Storage storage;
+    private Gson gson;
+
+    private Storage storage;
 
     private String market;
 
-    BittrexDataProviderTask(BittrexClient bittrexClient, Storage storage) {
+    BittrexDataProviderTask(BittrexClient bittrexClient, Gson gson) {
         this.bittrexClient = bittrexClient;
+        this.gson = gson;
+    }
+
+    BittrexDataProviderTask setStorage(Storage storage) {
         this.storage = storage;
+        return this;
     }
 
     BittrexDataProviderTask setMarket(String market) {
@@ -30,13 +40,17 @@ public class BittrexDataProviderTask implements Runnable {
     public void run() {
         try {
             // get data
-            String ticker = bittrexClient.getTicker(market);
-            System.out.println(String.format("[%s] %s %s", new Date(), market, ticker));
+            Instant instant = Instant.now();
+            long timeStamp = instant.getEpochSecond();
+            String resultRaw = bittrexClient.getTicker(market);
+            System.out.println(String.format("[%s] %s %s", new Date(), market, resultRaw));
+
+            // deserialize data
+            BittrexResult result = gson.fromJson(resultRaw, BittrexResult.class);
 
             // store data
-            storage.add(ticker);
-            // calculate indicators
-            // buy/sell
+            storage.add(new Ticker(timeStamp, result.getTicker().getLast()));
+
         } catch (BittrexClientException e) {
             e.printStackTrace();
         }
