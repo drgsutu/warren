443904d3fbc24e9ad7e677a8de2b1e86a810152d
Dragos Sutu <dragos.sutu@gmail.com>
2017-09-21 08:19:21 +0300

    Remove unused Bittrex v1 api classes.

--- src/main/java/io/sutu/DataProviders/Bittrex/BittrexMarkets.java
+++
@@ -1,7 +0,0 @@
-package io.sutu.DataProviders.Bittrex;
-
-public class BittrexMarkets {
-    public final static String BTCETH = "BTC-ETH";
-    public final static String BTCNEO = "BTC-NEO";
-    public final static String BTCOMG = "BTC-OMG";
-}
