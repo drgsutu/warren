2a225f1577f4c67e0b6c4b3662cf1105fde7fa3f
Dragos Sutu <dragos.sutu@gmail.com>
2017-09-22 07:49:27 +0300

    Schedule task to process data at 5 min interval with delay to start at minute :00 or :05.

--- src/main/java/io/sutu/Application.java
+++ src/main/java/io/sutu/Application.java
@@ -1,15 +1,24 @@
 package io.sutu;
 
+import io.sutu.DataProcessors.TickerAggregatorTask;
+import io.sutu.DataProcessors.TickerAggregatorTaskFactory;
 import io.sutu.DataProviders.CryptoCompare.SocketClient;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 class Application {
 
     private SocketClient socketClient;
+    private TickerAggregatorTaskFactory tickerAggregatorTaskFactory;
 
-    public Application(SocketClient socketClient) {
+    public Application(SocketClient socketClient, TickerAggregatorTaskFactory tickerAggregatorTaskFactory) {
         this.socketClient = socketClient;
+        this.tickerAggregatorTaskFactory = tickerAggregatorTaskFactory;
     }
 
     void run() {
@@ -18,6 +27,17 @@ class Application {
         };
 
         socketClient.subscribe(markets);
+
+        int cpuCores = Runtime.getRuntime().availableProcessors();
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(cpuCores);
+
+        long now = Instant.now().getEpochSecond();
+        short delay = (short)((now % 300) + 300);
+        int period = 300;
+        for (String market : markets) {
+            TickerAggregatorTask tickerAggregatorTask = tickerAggregatorTaskFactory.newTaskForMarket(market);
+            executorService.scheduleAtFixedRate(tickerAggregatorTask, delay, period, TimeUnit.SECONDS);
+        }
 
         // apply indicators on reformatted data
 
