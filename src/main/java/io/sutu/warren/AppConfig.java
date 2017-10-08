100cf033ca220e192a97c5a60646ff977ecd89fb
Dragos Sutu <dragos.sutu@gmail.com>
2017-10-08 22:13:24 +0300

    Add config library and default config file.

--- src/main/java/io/sutu/warren/AppConfig.java
+++ src/main/java/io/sutu/warren/AppConfig.java
@@ -1,6 +1,8 @@
 package io.sutu.warren;
 
 import com.google.gson.Gson;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("io.sutu.warren")
 public class AppConfig {
+
+    @Bean
+    public Config config() {
+        return ConfigFactory.load();
+    }
 
     @Bean
     public Gson gson() {
