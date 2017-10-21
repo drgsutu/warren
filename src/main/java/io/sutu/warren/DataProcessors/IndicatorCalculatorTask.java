7381c3f372110a527ff45fd28d286ffae43bd02f
Dragos Sutu <dragos.sutu@gmail.com>
2017-10-21 11:03:30 +0300

    Remove EMA debugging and fix Tick volume param.

--- src/main/java/io/sutu/warren/DataProcessors/IndicatorCalculatorTask.java
+++ src/main/java/io/sutu/warren/DataProcessors/IndicatorCalculatorTask.java
@@ -16,7 +16,7 @@ import java.util.concurrent.BlockingQueue;
 
 public class IndicatorCalculatorTask implements Runnable {
 
-    private static final int MINIMUM_TIME_FRAME = 25;
+    private static final int MINIMUM_TIME_FRAME = 26;
 
     private String market;
     private BlockingQueue<List<String>> OHLCVQueue;
@@ -38,7 +38,6 @@ public class IndicatorCalculatorTask implements Runnable {
 
         final ClosePriceIndicator closePriceIndicator = new ClosePriceIndicator(timeSeries);
         Indicator<Decimal> macd = new MACDIndicator(closePriceIndicator, 12, 26);
-        Indicator<Decimal> ema = new EMAIndicator(closePriceIndicator, 9);
         Indicator<Decimal> macdEma = new EMAIndicator(macd, 9);
         Rule entryRule = new CrossedUpIndicatorRule(macdEma, macd);
         Rule exitRule = new CrossedDownIndicatorRule(macdEma, macd);
@@ -55,7 +54,7 @@ public class IndicatorCalculatorTask implements Runnable {
                 lastProcessedOHLCVTimeStamp = timeStamp;
 
                 ZonedDateTime endTime = ZonedDateTime.ofInstant(Instant.ofEpochSecond(timeStamp), ZoneId.of("UTC"));
-                Tick tick = new BaseTick(endTime, ohlcv.get(1), ohlcv.get(2), ohlcv.get(3), ohlcv.get(4), ohlcv.get(5));
+                Tick tick = new BaseTick(endTime, ohlcv.get(1), ohlcv.get(2), ohlcv.get(3), ohlcv.get(4), ohlcv.get(6));
                 ticks.add(tick);
 
                 // do not calculate until we have the minimum historical data to calculate indicators
@@ -64,13 +63,14 @@ public class IndicatorCalculatorTask implements Runnable {
                 }
 
                 int index = ticks.size() - 1;
-                System.out.println(tick);
-                Decimal macdValue = macd.getValue(index);
-                Decimal macdEmaValue = macdEma.getValue(index);
+
+                final Decimal macdValue = macd.getValue(index);
+                final Decimal macdEmaValue = macdEma.getValue(index);
+                System.out.println(tick.getEndTime());
                 System.out.println(String.format("MACD %.10f", macdValue.toDouble()));
                 System.out.println(String.format("MEMA %.10f", macdEmaValue.toDouble()));
-                System.out.println(String.format("EMA  %.10f", ema.getValue(index).toDouble()));
                 System.out.println(String.format("DIFF %.10f", (macdValue.minus(macdEmaValue)).toDouble()));
+
                 if (strategy.shouldEnter(index)) {
                     // buy
                     System.out.println("Buy");
@@ -79,6 +79,7 @@ public class IndicatorCalculatorTask implements Runnable {
                     // sell
                     System.out.println("Sell");
                 }
+                System.out.println();
 
             } catch (InterruptedException e) {
                 System.out.println("Interrupted: " + getClass().getName());
